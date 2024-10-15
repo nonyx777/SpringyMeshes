ea4775f6f9e335bd4be68e4cc03a27e91c9eca53
nonyx777 <nahomretebo@gmail.com>
2024-10-15 21:52:54 +0300

    spring connection between the points

--- Assets/Scripts/SpringyMesh.cs
+++ Assets/Scripts/SpringyMesh.cs
@@ -7,15 +7,18 @@ using System.Linq;
 public class SpringyMesh : MonoBehaviour
 {
     Vector3[] vertices;
+    Vector3[] prevPos;
+    Vector3[] velocity;
+    Vector3[] acceleration;
     List<Cell> cells = new List<Cell>();
 
     public int size;
     public float space;
+    public float distance;
+    public float centerDistance;
 
-    public struct Cell
+    public class Cell
     {
-        //a -> d = lower
-        //e -> h = upper
         public int a, b, c, d, e, f, g, h;
         public Vector3 position;
 
@@ -33,9 +36,9 @@ public class SpringyMesh : MonoBehaviour
             this.position = (vertices[a] + vertices[b] + vertices[c] + vertices[d] + vertices[e] + vertices[f] + vertices[g] + vertices[h]) / 8;
         }
 
-        void setPosition(ref Vector3[] vertices)
+        public void setPosition(ref Vector3[] vertices)
         {
-            this.position = (vertices[a] + vertices[b] + vertices[c] + vertices[d] + vertices[e] + vertices[f] + vertices[g] + vertices[h]) / 8;
+            this.position = (vertices[this.a] + vertices[this.b] + vertices[this.c] + vertices[this.d] + vertices[this.e] + vertices[this.f] + vertices[this.g] + vertices[this.h]) / 8;
         }
     }
 
@@ -46,14 +49,112 @@ public class SpringyMesh : MonoBehaviour
     void Start()
     {
         Array.Resize(ref vertices, (size * size * size));
+        Array.Resize(ref prevPos, (size * size * size));
+        Array.Resize(ref velocity, (size * size * size));
+        Array.Resize(ref acceleration, (size * size * size));
+
+        assignToEmpty(ref vertices);
+        assignToEmpty(ref prevPos);
+        assignToEmpty(ref velocity);
+        assignToEmpty(ref acceleration);
+
         createGrid(size, space);
         attachCells();
+
+        int a = cells[0].a;
+        Vector3 p = cells[0].position;
+        distance = Vector3.Magnitude(vertices[a] - p);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        Vector3 force = Vector3.zero;
+        foreach(Cell cell in cells)
+        {
+            velocity[cell.a] += force * Time.fixedDeltaTime;
+            velocity[cell.b] += force * Time.fixedDeltaTime;
+            velocity[cell.c] += force * Time.fixedDeltaTime;
+            velocity[cell.d] += force * Time.fixedDeltaTime;
+            velocity[cell.e] += force * Time.fixedDeltaTime;
+            velocity[cell.f] += force * Time.fixedDeltaTime;
+            velocity[cell.g] += force * Time.fixedDeltaTime;
+            velocity[cell.h] += force * Time.fixedDeltaTime;
+        }
 
+        foreach(Cell cell in cells)
+        {
+            prevPos[cell.a] = vertices[cell.a];
+            prevPos[cell.b] = vertices[cell.b];
+            prevPos[cell.c] = vertices[cell.c];
+            prevPos[cell.d] = vertices[cell.d];
+            prevPos[cell.e] = vertices[cell.e];
+            prevPos[cell.f] = vertices[cell.f];
+            prevPos[cell.g] = vertices[cell.g];
+            prevPos[cell.h] = vertices[cell.h];
+        }
+
+        foreach(Cell cell in cells)
+        {
+            vertices[cell.a] += velocity[cell.a] * 0.1F * Time.fixedDeltaTime;
+            vertices[cell.b] += velocity[cell.b] * 0.1F * Time.fixedDeltaTime;
+            vertices[cell.c] += velocity[cell.c] * 0.1F * Time.fixedDeltaTime;
+            vertices[cell.d] += velocity[cell.d] * 0.1F * Time.fixedDeltaTime;
+            vertices[cell.e] += velocity[cell.e] * 0.1F * Time.fixedDeltaTime;
+            vertices[cell.f] += velocity[cell.f] * 0.1F * Time.fixedDeltaTime;
+            vertices[cell.g] += velocity[cell.g] * 0.1F * Time.fixedDeltaTime;
+            vertices[cell.h] += velocity[cell.h] * 0.1F * Time.fixedDeltaTime;
+        }
+
+        for(int i = 0; i < 1; i++)
+        {
+
+            foreach(Cell cell in cells)
+            {
+                applyDistanceConstraint(cell.a, cell.b, Time.fixedDeltaTime, space);
+                applyDistanceConstraint(cell.a, cell.c, Time.fixedDeltaTime, space);
+                applyDistanceConstraint(cell.a, cell.e, Time.fixedDeltaTime, space);
+                applyDistanceConstraint(cell.a, cell.position, Time.fixedDeltaTime, distance);
+
+                applyDistanceConstraint(cell.b, cell.d, Time.fixedDeltaTime, space);
+                applyDistanceConstraint(cell.b, cell.f, Time.fixedDeltaTime, space);
+                applyDistanceConstraint(cell.b, cell.position, Time.fixedDeltaTime, distance);
+
+                applyDistanceConstraint(cell.c, cell.d, Time.fixedDeltaTime, space);
+                applyDistanceConstraint(cell.c, cell.g, Time.fixedDeltaTime, space);
+                applyDistanceConstraint(cell.c, cell.position, Time.fixedDeltaTime, space);
+
+                applyDistanceConstraint(cell.d, cell.h, Time.fixedDeltaTime, space);
+                applyDistanceConstraint(cell.d, cell.position, Time.fixedDeltaTime, distance);
+
+                applyDistanceConstraint(cell.e, cell.g, Time.fixedDeltaTime, space);
+                applyDistanceConstraint(cell.e, cell.f, Time.fixedDeltaTime, space);
+                applyDistanceConstraint(cell.e, cell.position, Time.fixedDeltaTime, distance);
+
+                applyDistanceConstraint(cell.f, cell.h, Time.fixedDeltaTime, space);
+                applyDistanceConstraint(cell.f, cell.position, Time.fixedDeltaTime, distance);
+
+                applyDistanceConstraint(cell.g, cell.h, Time.fixedDeltaTime, space);
+                applyDistanceConstraint(cell.g, cell.position, Time.fixedDeltaTime, distance);
+            }
+        }
+
+        foreach(Cell cell in cells)
+        {
+            velocity[cell.a] = (vertices[cell.a] - prevPos[cell.a]) / Time.fixedDeltaTime;
+            velocity[cell.b] = (vertices[cell.b] - prevPos[cell.b]) / Time.fixedDeltaTime;
+            velocity[cell.c] = (vertices[cell.c] - prevPos[cell.c]) / Time.fixedDeltaTime;
+            velocity[cell.d] = (vertices[cell.d] - prevPos[cell.d]) / Time.fixedDeltaTime;
+            velocity[cell.e] = (vertices[cell.e] - prevPos[cell.e]) / Time.fixedDeltaTime;
+            velocity[cell.f] = (vertices[cell.f] - prevPos[cell.f]) / Time.fixedDeltaTime;
+            velocity[cell.g] = (vertices[cell.g] - prevPos[cell.g]) / Time.fixedDeltaTime;
+            velocity[cell.h] = (vertices[cell.h] - prevPos[cell.h]) / Time.fixedDeltaTime;
+        }
+
+        foreach(Cell cell in cells)
+        {
+            cell.setPosition(ref vertices);
+        }
     }
 
     void applyDistanceConstraint(Vector3 target, float deltaTime)
@@ -65,6 +166,34 @@ public class SpringyMesh : MonoBehaviour
         Vector3 forceVector = force * direction.normalized;
 
         vertices[0] -= forceVector * deltaTime;
+    }
+
+    void applyDistanceConstraint(int v1, int v2, float deltaTime, float distance_)
+    {
+        Vector3 p1 = vertices[v1];
+        Vector3 p2 = vertices[v2];
+
+        Vector3 direction = p2 - p1;
+        float currDis = direction.magnitude;
+
+        float force = currDis - distance_;
+        Vector3 forceVector = force * direction.normalized;
+
+        vertices[v1] += (0.5F * forceVector) * deltaTime;
+        vertices[v2] -= (0.5F * forceVector) * deltaTime;
+    }
+
+    void applyDistanceConstraint(int v1, Vector3 p, float deltaTime, float distance_)
+    {
+        Vector3 p1 = vertices[v1];
+
+        Vector3 direction = p - p1;
+        float currDis = direction.magnitude;
+
+        float force = currDis - distance_;
+        Vector3 forceVector = force * direction.normalized;
+
+        vertices[v1] += forceVector * deltaTime;
     }
 
     void OnDrawGizmos()
